177611695f0fa63ae15872fada911f62c155136d
VISVESSWAR <amvisvesswar2004@gmail.com>
2024-12-28 14:59:25 +0530

    switch save

--- mern-abacus-theme/frontend-new/src/components/Sponsors.js
+++ mern-abacus-theme/frontend-new/src/components/Sponsors.js
@@ -13,8 +13,8 @@ const Sponsors = () => {
 
   return (
     <div className="bg-[#f0f0f0] p-6" id="sponsors">
-      <h1 className="text-3xl font-bold text-black text-center mb-8">
-        Our Sponsors
+      <h1 className="text-5xl font-bold text-black text-center mb-8 overflow-hidden">
+        Sponsors
       </h1>
       <div className="flex flex-col justify-between lg:flex-row mx-auto overflow-clip bg-[#f0f0f0] w-[100%]">
         {sponsors.map((sponsor, index) => (
