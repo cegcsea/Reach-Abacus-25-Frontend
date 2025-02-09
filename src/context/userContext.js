c31a991d55db1085ba1244944d795cc25d380f53
Visvess <amvisvesswar2004@gmail.com>
2025-02-09 13:19:24 +0530

    bulk payment

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import { LoaderData } from "./loaderContext";
 // import { server } from "../main";
-//const server = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001";
+//const server = process.env.REACT_APP_API_BASE_URL ;
 const server = "http://localhost:3001";
 const UserContext = createContext();
 
