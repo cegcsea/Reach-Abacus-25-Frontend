82a46434c74f591531518d42022de3fb81a1c983
Harini Natarajan <harinirajan192004@gmail.com>
2025-03-04 20:33:09 +0530

    check events

--- src/constants/events.js
+++ src/constants/events.js
@@ -32,7 +32,7 @@ export const events = [
         path: "/events/ospc",
         Qr: true,
 
-        team: "Two or three members",
+        team: "2-3 members",
         Date: "21st March 2025",
         prize: "Rs 7k",
         to: "ospc",
@@ -75,8 +75,7 @@ export const events = [
           "Designed especially for first-year students, this event is the ultimate kickstart to their coding journey! Dive into the fundamentals of logical reasoning, arithmetic, and programming through interactive challenges that are both fun and beginner-friendly. Get ready to think, solve, and code your way to success!",
         image: img5,
         path: "/events/byte-begin",
-
-        team: "One or two members",
+        team: "1-2 members",
         Date: "21st February 2025",
         prize: "Rs 7k",
         to: "byte-begin",
@@ -118,7 +117,7 @@ export const events = [
           "Dive into the world of machine learning in this thrilling event! Test your knowledge, solve challenges, and showcase your skills by developing innovative solutions to given problems. Compete with the best and prove your expertise in ML!",
         image: img6, // Replace with your image path
         path: "/events/brainwave.ml",
-        team: "Two or three members",
+        team: "1-2 members",
         Date: "21st February 2025",
         prize: "Rs 5k",
         to: "brainwave.ml",
@@ -159,7 +158,7 @@ export const events = [
           "Ctrl+Escape is a dynamic fusion of a tech quiz and coding challenges, designed to test both your knowledge and programming skills! Solve intriguing quiz questions to unlock coding tasks, where logic and speed are key. Get ready for an electrifying battle of wit and code!",
         image: img6, // Replace with your image path
         path: "/events/ctrl+escape",
-        team: "Two or three members",
+        team: "2-3 members",
         Date: "21st February 2025",
         prize: "Rs 7k",
         to: "ctrl+escape",
@@ -239,7 +238,7 @@ export const events = [
           "Ready. Set. Hire! is your ultimate gateway to the real-world hiring experience! Dive into a competitive coding round, followed by a mock interview that mirrors actual recruitment challenges. Test your technical skills, sharpen your problem-solving abilities, and gain invaluable hands-on experience to ace future job interviews!",
         image: img6, // Replace with your image path
         path: "/events/ready-set-hire",
-        team: "Two or three members",
+        team: "Individual",
         Date: "21st February 2025",
         prize: "Rs 7k",
         to: "ready-set-hire",
@@ -279,7 +278,7 @@ export const events = [
           "Glitch Snitch is an exciting challenge that puts your debugging skills to the test! Hunt down and fix bugs in pre-written code, tackling real-world coding issues under time pressure. Sharpen your problem-solving abilities and level up your coding expertise in this fast-paced, brain-teasing event!",
         image: img6, // Replace with your image path
         path: "/events/glitch-snitch",
-        team: "Two or three members",
+        team: "1-2 members",
         Date: "21st February 2025",
         prize: "Rs 5k",
         to: "glitch-snitch",
@@ -326,9 +325,9 @@ export const events = [
         title: "TREASURE HUNT",
         time: "01:30 PM to 04:00 PM",
         Date: "20 February 2025",
-        team: "Three members",
+        team: "2-3 members",
         description:
-          "Join the Treasure Hunt for an adventurous campus quest! Teams will receive clues leading them to hidden locations around the campus, each revealing the next clue to uncover further secrets. The fastest team to navigate all the clues and complete the hunt will win the ultimate prize—get ready for an exciting race of wits and speed!",
+          "Embark on an exhilarating campus-wide Treasure Hunt! Solve clever clues, uncover hidden locations, and race against time to crack the final mystery. Teams will navigate through a series of challenges, with the fastest ones claiming victory. Get ready for a thrilling adventure of wits, strategy, and speed!",
         image: img7, // Replace with your image path
         path: "/events/treasure-hunt",
         rules: `• Participants answer survey-based questions on topics like Books, Food, Movies, Lifestyle, and Technology. Responses are ranked (1 = most popular, 7 = least popular).
@@ -339,16 +338,28 @@ export const events = [
               • Each round includes 10 questions.
               • Top 3 teams with the highest total scores win.`,
         to: "treasure-hunt",
-        prize: "Rs 1k+",
+        prize: "Rs 2.5k",
+        contact: [
+          {
+            id: 1,
+            name: "Sahana S",
+            phone: "74182 47031",
+          },
+          {
+            id: 2,
+            name: "Harini S",
+            phone: "93633 49994",
+          },
+        ],
       },
       {
         id: 9,
         title: "IPL AUCTION",
         time: "01:30 PM to 04:00 PM",
         Date: "20 February 2025",
-        team: "Three members",
+        team: "1-3 members",
         description:
-          "IPL Auction lets you create your dream team by selecting players who can lead your squad to victory. Strategize, choose wisely, and keep your team in the race for the long haul of the tournament!",
+          "IPL Auction gives you the ultimate chance to build your dream team! Strategize, bid smartly, and assemble a winning squad that can dominate the tournament. Choose wisely—every pick counts in this high-stakes game of skill and strategy!",
         image: img7, // Replace with your image path
         path: "/events/ipl-auction",
         rules: `• Participants answer survey-based questions on topics like Books, Food, Movies, Lifestyle, and Technology. Responses are ranked (1 = most popular, 7 = least popular).
@@ -359,16 +370,28 @@ export const events = [
               • Each round includes 10 questions.
               • Top 3 teams with the highest total scores win.`,
         to: "ipl-auction",
-        prize: "Rs 1k+",
+        prize: "Rs 2.5k",
+        contact: [
+          {
+            id: 1,
+            name: "Sreenithika S",
+            phone: "99650 65777",
+          },
+          {
+            id: 2,
+            name: "Infancy P",
+            phone: "90473 66527",
+          },
+        ],
       },
       {
         id: 10,
         title: "CHATGPT PULSE",
         time: "01:30 PM to 04:00 PM",
         Date: "20 February 2025",
-        team: "Three members",
+        team: "1-2 members",
         description:
-          "Put your prompt engineering skills to the ultimate test! In this thrilling event, participants get just 5 chances to craft the perfect prompts that guide ChatGPT to generate a specific target word. Unleash your creativity, sharpen your logic, and master the art of AI communication in this fun, interactive, and brain-teasing challenge.",
+          "Put your prompt engineering skills to the test in this exciting AI challenge! With only a few chances, craft the perfect prompts to steer ChatGPT toward a specific target word. Unleash your creativity, refine your logic, and master the art of AI communication in this fun and brain-teasing competition.",
         image: img7, // Replace with your image path
         path: "/events/chatgpt-pulse",
         rules: `• Participants answer survey-based questions on topics like Books, Food, Movies, Lifestyle, and Technology. Responses are ranked (1 = most popular, 7 = least popular).
@@ -379,10 +402,54 @@ export const events = [
               • Each round includes 10 questions.
               • Top 3 teams with the highest total scores win.`,
         to: "chatgpt-pulse",
-        prize: "Rs 1k+",
+        prize: "Rs 2k",
+        contact: [
+          {
+            id: 1,
+            name: "Renjitha K",
+            phone: "79078 54731",
+          },
+          {
+            id: 2,
+            name: "Ajai Krishna T.S",
+            phone: "70107 07652",
+          },
+        ],
+      },
+      {
+        id: 11,
+        title: "Checkmate Chronicles (Collab with Castle Red)",
+        time: "01:30 PM to 04:00 PM",
+        Date: "20 February 2025",
+        team: "Individual",
+        description:
+          "Checkmate Chronicles is the ultimate test of strategy, logic, and precision! Outsmart your opponents in intense chess battles where every move shapes your path to victory. Sharpen your tactics, anticipate your rival’s next move, and compete for the title of Chess Champion!",
+        image: img7, // Replace with your image path
+        path: "/events/chatgpt-pulse",
+        rules: `• Participants answer survey-based questions on topics like Books, Food, Movies, Lifestyle, and Technology. Responses are ranked (1 = most popular, 7 = least popular).
+              • Teams compete to guess the most popular answers.
+              • Buzzers decide who answers first.
+              • Correct guesses earn points based on the popularity ranking.
+              • Three consecutive wrong guesses result in negative points, and the chance passes to the other team.
+              • Each round includes 10 questions.
+              • Top 3 teams with the highest total scores win.`,
+        to: "chatgpt-pulse",
+        prize: "Rs 3.5k",
+        contact: [
+          {
+            id: 1,
+            name: "Hariharan A",
+            phone: "77084 62392",
+          },
+          {
+            id: 2,
+            name: "Dinesh S",
+            phone: "93603 80769",
+          },
+        ],
       },
     ],
-    // Add more events h
+    // Add more events 
     path: "/events/non-technical-events", // Path to the event page
   },
   {
@@ -395,8 +462,23 @@ export const events = [
         description:
           "OLPC is the ultimate arena for online programming contests, where sharp minds battle against the clock to solve intricate challenges packed with complex data structures and algorithms. Speed, precision, and problem-solving skills will determine who emerges victorious in this high-stakes digital showdown!",
         image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "Individual",
+        prize: "Rs 4k",
         path: "/events/olpc",
         to: "olpc",
+        contact: [
+          {
+            id: 1,
+            name: "Harini S",
+            phone: "93633 49994",
+          },
+          {
+            id: 2,
+            name: "Infancy P",
+            phone: "90473 66527",
+          },
+        ],
       },
 
       {
@@ -405,8 +487,23 @@ export const events = [
         description:
           "Unleash your creativity and showcase your talent in this ultimate reels competition! Whether it’s dance, drama, comedy, or art—hit record, shine bright, and make your moment go viral! ",
         image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "Individual",
+        prize: "Rs 1.5k",
         path: "/events/lights-camera-reel",
         to: "lights-camera-reel",
+        contact: [
+          {
+            id: 1,
+            name: "Sreenithika S",
+            phone: "99650 65777",
+          },
+          {
+            id: 2,
+            name: "Sahana S",
+            phone: "74182 47031",
+          },
+        ],
       },
       {
         id: 13,
@@ -414,8 +511,23 @@ export const events = [
         description:
           "War of the Rackets is an electrifying badminton showdown exclusively for girls! Battle it out in intense, fast-paced matches where agility, precision, and strategy take center stage. Smash your way to victory and claim your spot at the top in this ultimate test of skill and endurance!",
         image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "Individual (Inter dept)",
+        prize: "Rs 1.5k",
         path: "/events/war-of-the-rackets",
         to: "war-of-the-rackets",
+        contact: [
+          {
+            id: 1,
+            name: "Renjitha K",
+            phone: "79078 54731",
+          },
+          {
+            id: 2,
+            name: "Sahana S",
+            phone: "74182 47031",
+          },
+        ],
       },
       {
         id: 14,
@@ -423,8 +535,23 @@ export const events = [
         description:
           "Grand Cricket League is the ultimate battle of skill, strategy, and endurance! Compete in high-energy matches where every run matters, every wicket counts, and teamwork paves the path to victory. Step onto the pitch, showcase your batting, bowling, and fielding prowess, and chase the glory of becoming the champions!",
         image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "11 members (Intra Dept)",
+        prize: "Rs 2k",
         path: "/events/gcl",
         to: "gcl",
+        contact: [
+          {
+            id: 1,
+            name: "Hariharan A",
+            phone: "77084 62392",
+          },
+          {
+            id: 2,
+            name: "Dinesh S",
+            phone: "93603 80769",
+          },
+        ],
       },
       {
         id: 15,
@@ -432,8 +559,47 @@ export const events = [
         description:
           "Lace up your boots and get ready for an electrifying intra-departmental football showdown! Form your dream team, showcase your dribbling, passing, and scoring skills, and battle your way to glory in this fast-paced tournament of teamwork, strategy, and passion for the game!",
         image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "5 members (Intra Dept)",
+        prize: "Rs 2k",
         path: "/events/gcl",
         to: "gcl",
+        contact: [
+          {
+            id: 1,
+            name: "Hariharan A",
+            phone: "77084 62392",
+          },
+          {
+            id: 2,
+            name: "Ajai Krishna T.S",
+            phone: "70107 07652",
+          },
+        ],
+      },
+      {
+        id: 16,
+        title: "CHESS!",
+        description:
+          "",
+        image: img1, // Replace with your image path
+        Date: "20 February 2025",
+        team: "5 members (Intra Dept)",
+        prize: "Rs 30   (need to change price as entry fee)",
+        path: "/events/chess",
+        to: "gcl",
+        contact: [
+          {
+            id: 1,
+            name: "Hariharan A",
+            phone: "77084 62392",
+          },
+          {
+            id: 2,
+            name: "Ajai Krishna T.S",
+            phone: "70107 07652",
+          },
+        ],
       },
     ],
     path: "/events/technical-events", // Path to the event page
