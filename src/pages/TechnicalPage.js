f66a63883b73481d41868af5492346f2da770a79
sivasanjaycse <sivasanjaidisco@gmail.com>
2026-02-06 19:27:16 +0530

    Chnage the inconsistent themes of events and added coming soon page to workshop

--- src/pages/TechnicalPage.js
+++ src/pages/TechnicalPage.js
@@ -13,22 +13,20 @@ const TechnicalPage = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   //const index = 1; // Define the variable before using it
   //console.log(index); // Use it after definition
-  const location=useLocation();
+  const location = useLocation();
   const { event } = location.state || {};
-  
-  const { isLoading } =LoaderData();
+
+  const { isLoading } = LoaderData();
 
   if (isLoading) {
     return <Loader />;
   }
   return (
-    <div className="bg-black">
+    <>
       <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-      <div style={{ backgroundColor: "#000", padding: "20px" }}>
+      <div style={{ padding: "20px" }}>
         <div className="heading">
-          <h1 style={{ textAlign: "center", color: "#fff" }}>
-            {event.title}
-          </h1>
+          <h1 style={{ textAlign: "center", color: "#fff" }}>{event.title}</h1>
         </div>
         <div className="out">
           {event?.event?.map((event, index) => (
@@ -44,19 +42,19 @@ const TechnicalPage = () => {
                 <p>{event.description}</p>
                 <Link
                   to={event.path} // Dynamic link to the event's detailed page
-                  state={{ event }} 
-                  className="lg:mt-10 mx-7 lg:mb-6  px-4 py-2 lg:px-6 lg:py-4 bg-gradient-to-br from-red-300 via-red-500 to-red-800 text-white font-bold rounded-full shadow-lg hover:shadow-[0_0_10px_rgba(255,255,255,0.8)] transition-shadow focus:outline-none focus:shadow-[0_0_15px_rgba(255,223,47,0.8)]"
+                  state={{ event }}
+                  className="lg:mt-10 mx-7 lg:mb-6  px-4 py-2 lg:px-6 lg:py-4 bg-gradient-to-r from-[#c0a068] via-[#aa8c2c] to-[#66541a] text-white font-bold rounded-full shadow-lg hover:shadow-[0_0_10px_rgba(255,255,255,0.8)] transition-shadow focus:outline-none focus:shadow-[0_0_15px_rgba(255,223,47,0.8)]"
                   style={{ marginTop: "20px", marginLeft: "-1px" }}
                 >
-                  Read More &lt;~&gt;
+                  Read More
                 </Link>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <Contact contacts={EventContacts}/>
-    </div>
+      <Contact contacts={EventContacts} />
+    </>
   );
 };
 
