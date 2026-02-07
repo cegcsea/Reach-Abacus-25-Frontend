7d85e511e95558e4530251ce9184ce8e89076444
harinii19 <harinirajan192004@gmail.com>
2026-02-08 00:17:37 +0530

    ca done

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserData } from "../context/userContext";
 import { FaLocationDot } from "react-icons/fa6";
