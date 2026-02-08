b2fb757b276541fd66c8a841c5f4b6989f927c40
cegcsea <cseaceg.tech@gmail.com>
2026-02-08 11:03:04 +0530

    corected

--- src/utils/register_ambassador.js
+++ src/utils/register_ambassador.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const checkCA20Events = async (userId) => {
   try {
-    await axios.post("/user/check-ca", { userId });
+    await axios.post("/admin/check-ca", { userId });
   } catch (err) {
     console.log("CA check failed");
   }
