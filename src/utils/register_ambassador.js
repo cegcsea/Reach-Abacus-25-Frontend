9452d8f6d8d018bfdd10bd2f1d9d1cacb0feff1c
cegcsea <cseaceg.tech@gmail.com>
2026-02-08 11:09:08 +0530

    env change

--- src/utils/register_ambassador.js
+++ src/utils/register_ambassador.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export const checkCA20Events = async (userId) => {
   try {
-    await axios.post("/admin/check-ca", { userId });
+
+await axios.post(
+  `${process.env.REACT_APP_API_BASE_URL}/admin/check-ca`,
+  { userId }
+);
   } catch (err) {
     console.log("CA check failed");
   }
